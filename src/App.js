650454d6eec7ac3093ef597c6c87bde79d80360e
omegamaster86 <masya868686@gmail.com>
2023-01-31 00:05:53 +0900

    Mainコンポーネント化

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Main from "./components/Main";
 
 function App() {
   return (
     <div style={{padding: "50px"}}>
       <h1 style={{marginBottom: "20px"}}>タスク管理アプリ</h1>
+      <Main />
     </div>
   );
 }
